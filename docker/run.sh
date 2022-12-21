d25db104a01eb3669229e76360a9ad7aaae782b2
kwy <kwyoung04@naver.com>
2022-12-21 14:21:55 +0900

    Docker update cmd

--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,8 @@
 docker run  -it \
             --gpus all \
-            --rm \
+            -d \
             -v /home/ubuntu/eric/Swin-Transformer-Object-Detection:$HOME/src/swint \
-            -v /data:$HOME/data \
+            -v /data/nia/4-2_221221/:$HOME/data \
             --env="DISPLAY" \
             --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
             --name $2 \
