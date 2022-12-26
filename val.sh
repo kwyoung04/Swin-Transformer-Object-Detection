e9decb6b1597648f5eeba82630124775e4e6cae1
kwy <kwyoung04@naver.com>
2022-12-26 18:44:29 +0900

    End of version in docker

--- val.sh
+++ val.sh
@@ -1,1 +1,7 @@
-python tools/test.py configs/swin/nia_zeron.py work_dirs/nia_zeron/latest.pth --eval bbox segm
+sudo /opt/conda/bin/python setup.py develop
+pip install -r requirements.txt
+## sudo /opt/conda/bin/pip uninstall pycocotools
+## pip install mmpycocotools
+## MMCV_WITH_OPS=1 FORCE_CUDA=1 pip install mmcv-full==1.4.0
+
+python tools/test.py configs/swin/nia_zeron.py work_dirs/nia_zeron/epoch_10.pth --eval bbox segm
