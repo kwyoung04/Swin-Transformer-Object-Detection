46218a0b23cf4027f545b119fbfd0ed021671080
kwy <kwyoung04@naver.com>
2023-09-06 15:37:00 +0900

    Temporary fix for width height bug

--- tools/nia_parsing.py
+++ tools/nia_parsing.py
@@ -1,6 +1,10 @@
 import json
 import os
+from PIL import Image
 from tqdm import tqdm
+
+import tkinter as tk
+from tkinter import filedialog
 
 def find_json_files_in_folder(folder_path):
     json_files = []
@@ -46,10 +50,20 @@ def json_ummary(coco_data):
             csv_file.write(f'{class_id},{class_name}\n')
     return 1
     
-
-abs_path = "/home/ubuntu/data/train/fix_test/annotation/"
+def get_image_size(image_path):
+    try:
+        with Image.open(image_path) as img:
+            width, height = img.size
+            return width, height
+    except IOError:
+        print(f"Unable to open image at {image_path}")
+        return None, None
+    
+    
+abs_path = "/home/ubuntu/data/train/fix_test/sample/"
+img_dir_path = "/home/ubuntu/data/train/fix_test/image/"
 json_files = find_json_files_in_folder(abs_path)
-coco_file_path = "fixed_anno.json"
+coco_file_path = "fixed_sample.json"
 
 coco_data = {
     "images": [],
@@ -59,13 +73,24 @@ coco_data = {
 }
 
 category_id = 1
+cnt_id = 0
 category_name_to_id = {}
 
+
 for json_file in tqdm(json_files, desc="Processing", unit="json_file"):
+    if json_file == 'VID_0082600_person(person)_(4_2).json':
+        print(json_file)
     with open(abs_path+json_file, "r") as f:
         data = json.load(f)
         
         for image_info in data["images"]:
+            #width, height = get_image_size(img_dir_path+image_info['file_name'])
+            #image_info['width'] = width
+            #image_info['height'] = height
+            
+            if image_info['width'] == 1072:
+                image_info['width'] = 1080
+            image_info['id'] = int(image_info['id'])
             coco_data["images"].append(image_info)
         
         for category in data["categories"]:
@@ -80,6 +105,8 @@ for json_file in tqdm(json_files, desc="Processing", unit="json_file"):
                 category_id += 1
         
         for annotation in data["annotations"]:
+            annotation['id'] = cnt_id
+            cnt_id += 1
             key_to_find = annotation["category_id"]
             for tmp_category in coco_data["tmp"]:
                 if str(key_to_find) == next(iter(tmp_category)):
